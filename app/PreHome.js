873d1b06fd6187ed596b79518e12e486a92f4df2
NiladriChatterje <radongtxgpu@gmail.com>
2023-07-09 02:20:28 +0530

    first but almost final commit

---
+++ app/PreHome.js
@@ -0,0 +1,14 @@
+import { SafeAreaView, Text, TextInput } from 'react-native';
+import { useContext } from 'react';
+
+const PreHome = () => {
+    return (
+        <SafeAreaView>
+            <View>
+                <Text>Name :</Text>
+                <TextInput />
+
+            </View>
+        </SafeAreaView>
+    )
+}
---
+++ hook/useFetch.js
@@ -0,0 +1,48 @@
+import { useState, useEffect } from "react";
+import axios from "axios";
+import { REACT_APP_API_KEY } from '@env';
+
+const useFetch = (endpoint, query) => {
+  const [data, setData] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState(null);
+
+  const options = {
+    method: "GET",
+    url: `https://jsearch.p.rapidapi.com/${endpoint}`,
+    headers: {
+      "X-RapidAPI-Key": '3224681278mshdf603cd43fde3cbp13a2f6jsnf3ec618192c1',
+      "X-RapidAPI-Host": "jsearch.p.rapidapi.com",
+    },
+    params: { ...query },
+  };
+
+  const fetchData = async () => {
+    setIsLoading(true);
+
+    try {
+      const { data } = await axios.request(options);
+
+      setData(data.data);
+      setIsLoading(false);
+    } catch (error) {
+      setError(error);
+      console.log(error)
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  const refetch = () => {
+    setIsLoading(true);
+    fetchData();
+  };
+
+  return { data, isLoading, error, refetch };
+};
+
+export default useFetch;
